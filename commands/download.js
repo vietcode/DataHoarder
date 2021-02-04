b1194f3266e3377c890768b1fd4b080374a40ba6
Son Tran-Nguyen <github@sntran.com>
2021-02-03 21:20:36 -0600

    feat: show link to uploaded file as embed

--- commands/download.js
+++ commands/download.js
@@ -6,6 +6,13 @@ const fetch = require("node-fetch");
 const rclone = require("../bin/rclone.js");
 const { bytes, progress } = require("../utils.js");
 
+const {
+  RCLONE_CONFIG_TARGET_TEAM_DRIVE,
+  RCLONE_CONFIG_TARGET_ROOT_FOLDER_ID,
+} = process.env;
+
+const folder = RCLONE_CONFIG_TARGET_ROOT_FOLDER_ID || RCLONE_CONFIG_TARGET_TEAM_DRIVE;
+
 module.exports = {
   name: "download",
   aliases: [],
@@ -68,7 +75,28 @@ module.exports = {
     });
 
     rcat.stdout.on("end", () => {
-      reply.edit(`${ header }\n**Status**: Uploaded.`);
+      // Retrieves ID of the new file.
+      const lsf = rclone.lsf(`target:${ filename }`, "--format", "i");
+      let id = "";
+
+      lsf.stdout.on("data", data => {
+        id += data;
+      });
+
+      lsf.stdout.on("end", () => {
+        const author = message.author;
+
+        const downloadEmbed = new Discord.MessageEmbed()
+          .setTitle(filename)
+          .setURL(`https://drive.google.com/file/d/${ id.trim() }`)
+          .setAuthor(author.username, author.displayAvatarURL())
+          .setDescription(`Size: ${ bytes(fileSize) }\n[Folder](https://drive.google.com/drive/folders/${ folder })`)
+          .setTimestamp();
+
+        reply.edit(`File uploaded:`, {
+          embed: downloadEmbed,
+        });
+      });
     });
 
     response.body.pipe(rcat.stdin);
